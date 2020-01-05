22ac42c7b752abdda88de756efe333c62d9418d1
Kazunari Takagi <kaztakag@yahoo-corp.jp>
2020-01-05 17:37:42 +0900

    fix to use config.js for hiding Confidentials.

--- src/main.js
+++ src/main.js
@@ -4,12 +4,20 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import firebase from 'firebase'
+import firebaseEnv from './config/firebaseEnv'
 
 Vue.config.productionTip = false
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // Confidentials
+  apiKey: firebaseEnv.apiKey,
+  authDomain: firebaseEnv.authDomain,
+  databaseURL: firebaseEnv.databaseURL,
+  projectId: firebaseEnv.projectId,
+  storageBucket: firebaseEnv.storageBucket,
+  messagingSenderId: firebaseEnv.messagingSenderId,
+  appId: firebaseEnv.appId,
+  measurementId: firebaseEnv.measurementId
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
